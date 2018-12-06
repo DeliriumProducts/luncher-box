9abe3cd78fb6253d5afa53a316420f45fcac2dac
Simo Aleksandrov <simo3003@me.com>
2018-12-06 23:12:23 +0200

    Init next-app

---
+++ frontend/pages/index.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import { Component, Fragment } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        Welcome to Luncher Box!
+      </div>
+    );
+  }
+}
+export default Home;
