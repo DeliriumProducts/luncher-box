7ff0d3e72e741187272cee46bffc1dcafefa5622
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-12-25 18:14:48 +0200

    Export api calls to an external API module

---
+++ frontend/api/category.ts
@@ -0,0 +1,55 @@
+import axios from 'axios';
+import { BACKEND_URL } from './../config';
+import { Category } from '../types';
+
+export class CategoryAPI {
+  static opts = {
+    withCredentials: true
+  };
+
+  static async create(category: Category) {
+    const response = await axios.post(
+      `${BACKEND_URL}/categories`,
+      category,
+      this.opts
+    );
+
+    return response;
+  }
+
+  static async edit(category: Category) {
+    const { id } = category;
+    const response = await axios.put(
+      `${BACKEND_URL}/categories/${id}`,
+      category,
+      this.opts
+    );
+
+    return response;
+  }
+
+  static async delete({ id }: Category) {
+    const response = await axios.delete(
+      `${BACKEND_URL}/categories/${id}`,
+      this.opts
+    );
+
+    return response;
+  }
+
+  static async getAll() {
+    const categories: Category[] = (await axios.get(
+      `${BACKEND_URL}/categories`
+    )).data;
+
+    return categories;
+  }
+
+  static async getOne({ id }: Category) {
+    const category: Category = (await axios.get(
+      `${BACKEND_URL}/categories/${id}`
+    )).data;
+
+    return category;
+  }
+}
