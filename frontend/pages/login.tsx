9661a8e8bb1dcbebc5f7896e80251735c6cf05e0
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-12-26 01:35:04 +0200

    Remove password pattern validation for login

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -96,7 +96,6 @@ class LoginForm extends React.Component<Props, State> {
                 rules: [
                   {
                     required: true,
-                    pattern: /^(?=.*[a-z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
                     message:
                       // tslint:disable-next-line
                       'Password must contain at least 1 lowercase alphabetical character, 1 special symbol, 1 numeric character and be at least 8 characters long'
