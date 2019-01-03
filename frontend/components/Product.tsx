41c0dc5dd282e1928e7283607846b64cac8f8483
Simo Aleksandrov <simo3003@me.com>
2019-01-03 23:34:58 +0200

    Add button for adding into cart

--- frontend/components/Product.tsx
+++ frontend/components/Product.tsx
@@ -1,39 +1,95 @@
 import styled from 'styled-components';
-import { Card } from 'antd';
+import { Card, Button, Icon } from 'antd';
 
 const { Meta } = Card;
 
 const StyledCard = styled(Card)`
-  min-width: 256px;
-  max-width: 512px;
   border-radius: 7px;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
   margin: 8px 4px 0 4px;
   box-shadow: 0 4px 4px rgba(0, 0, 0, 0.12);
+  width: 16rem;
+  min-height: 25rem;
+  padding-bottom: 0;
+
+  .ant-card-body {
+    padding-top: 0;
+  }
+
   & img {
     border-radius: 7px 7px 0 0;
-    max-height: 128px;
     object-fit: cover;
-    display: cover;
+    width: 100%;
+    height: 16rem;
+  }
+
+  @media (max-width: 768px) {
+    border-bottom: 1px solid;
+    border-bottom-color: rgb(210, 210, 210);
+    box-shadow: none;
+    margin: 8px 0 0 0;
+    width: 100%;
   }
 `;
 
 const StyledMeta = styled(Meta)`
   display: flex;
+  flex-grow: 1;
   align-items: center;
+  justify-content: center;
   & * {
     white-space: initial;
+    overflow-wrap: normal;
   }
 `;
 
-type Props = {
+interface Props {
   name: string;
   image: string;
   description: string;
   price: string;
+}
+
+const addDots = (str: string, limit: number) => {
+  if (str.length > limit) {
+    return [str.substring(0, limit) + '...', true];
+  }
+
+  return [str, false];
 };
 
-export default (props: Props) => (
-  <StyledCard bordered={false} cover={<img alt="example" src={props.image} />}>
-    <StyledMeta title={props.name} description={props.description} />
-  </StyledCard>
-);
+export default (props: Props) => {
+  const [shortName, readMoreName] = addDots(props.name, 50);
+  const [shortDesc, readMoreDesc] = addDots(props.description, 150);
+
+  return (
+    <StyledCard
+      bordered={false}
+      cover={
+        <div>
+          <img alt="example" src={props.image} />
+          <Button
+            style={{
+              width: 44,
+              height: 44,
+              zIndex: 50,
+              position: 'relative',
+              bottom: 22
+            }}
+            icon="plus"
+            shape="circle"
+          />
+        </div>
+      }
+    >
+      <StyledMeta title={shortName} description={<p>{shortDesc}</p>} />
+      {(readMoreName || readMoreDesc) && (
+        <Button style={{}} size="small" icon="info">
+          View more
+        </Button>
+      )}
+    </StyledCard>
+  );
+};
--- frontend/components/UserLayout.tsx
+++ frontend/components/UserLayout.tsx
@@ -1,6 +1,7 @@
 import { Affix, Layout } from 'antd';
 import React, { ReactNode } from 'react';
 import UserMenuBar from './UserMenuBar';
+import styled from 'styled-components';
 
 const { Content } = Layout;
 
@@ -10,24 +11,20 @@ interface Props {
   img?: string;
 }
 
+const StyledContent = styled(Content)`
+  padding: 50px;
+  @media (max-width: 768px) {
+    padding: 20px;
+  }
+`;
+
 const UserLayout: React.FunctionComponent<Props> = props => {
   return (
     <Layout>
       <Affix offsetTop={0}>
         <UserMenuBar selectedKey={props.selectedKey} />
       </Affix>
-      <Content>
-        <div
-          style={{
-            minHeight: '100vh',
-            paddingBottom: 5,
-            paddingLeft: 10,
-            paddingRight: 10
-          }}
-        >
-          {props.children}
-        </div>
-      </Content>
+      <StyledContent>{props.children}</StyledContent>
     </Layout>
   );
 };
