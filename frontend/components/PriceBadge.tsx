33bd64aef97ac309b5f81a176bcc68a3ad5e1e4d
Lyubo Lyubchev <lyubo_2317@abv.bg>
2019-01-04 20:04:18 +0200

    Refactor PriceBadge component

--- frontend/components/PriceBadge.tsx
+++ frontend/components/PriceBadge.tsx
@@ -1,10 +1,26 @@
 import { Badge } from 'antd';
-import styled from 'styled-components';
 
-const PriceBadge = styled(Badge)`
-  background-color: #fff;
-  color: rgba(0, 0, 0, 0.65);
-  box-shadow: 0 0 0 1px #d9d9d9 inset;
-`;
+interface Props {
+  count?: React.ReactNode;
+  dot?: boolean;
+  offset?: [number, number];
+  overflowCount?: number;
+  showZero?: boolean;
+  text?: string;
+  title?: string;
+}
+
+const PriceBadge = (props: Props) => {
+  return (
+    <Badge
+      {...props}
+      style={{
+        backgroundColor: '#fff',
+        color: 'rgba(0, 0, 0, 0.65)',
+        boxShadow: '0 0 0 1px #d9d9d9 inset'
+      }}
+    />
+  );
+};
 
 export default PriceBadge;
