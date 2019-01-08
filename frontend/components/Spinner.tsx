546a84879b4cf650cd10f663978afce974591f20
Lyubo Lyubchev <lyubo_2317@abv.bg>
2019-01-08 10:20:07 +0200

    Fix Spinner component

--- frontend/components/Spinner.tsx
+++ frontend/components/Spinner.tsx
@@ -1,7 +1,7 @@
 import { Spin, Icon } from 'antd';
 
-const Spinner = () => {
-  <Spin indicator={<Icon style={{ color: '#fff' }} type="loading" spin />} />;
-};
+const Spinner = () => (
+  <Spin indicator={<Icon style={{ color: '#fff' }} type="loading" spin />} />
+);
 
 export default Spinner;
