4c247c6acf7d3c61d69614527ce038100dc8ca2b
Lyubo Lyubchev <lyubo_2317@abv.bg>
2019-01-08 14:52:16 +0200

    Fix responsiveness for home page

--- frontend/pages/admin/index.tsx
+++ frontend/pages/admin/index.tsx
@@ -1,10 +1,16 @@
 import { Component } from 'react';
+import { message } from 'antd';
 import AdminLayout from '../../components/AdminLayout';
 import withAuth from '../../components/withAuth';
 import { EntityContext } from '../../context';
 import EntityCard from '../../components/EntityCard';
 import styled from 'styled-components';
 import EntityCardContainer from '../../components/EntityCardContainer';
+import Spinner from '../../components/Spinner';
+
+interface State {
+  loading: boolean;
+}
 
 const FlexContainer = styled.div`
   display: flex;
@@ -16,19 +22,33 @@ const FlexContainer = styled.div`
     flex: 1;
     max-width: 49%;
     height: 100%;
+  }
 
-    @media (max-width: 768px) {
+  @media (max-width: 768px) {
+    .col {
       max-width: 100%;
       margin-top: 3%;
     }
+
+    flex-direction: column;
   }
 `;
-class Index extends Component {
+class Index extends Component<any, State> {
   static contextType = EntityContext;
   context!: React.ContextType<typeof EntityContext>;
 
+  state = {
+    loading: true
+  };
+
   async componentDidMount() {
-    await this.context.actions.update();
+    try {
+      await this.context.actions.update();
+    } catch (err) {
+      message.error(`${err}`);
+    } finally {
+      this.setState({ loading: false });
+    }
   }
 
   render() {
@@ -40,35 +60,34 @@ class Index extends Component {
               title={`Categories (${this.context.entities.categories.length})`}
               entityType="category"
             >
-              {this.context.entities.categories.map(category => (
+              {this.context.entities.categories &&
+                this.context.entities.categories.map(category => (
+                  <EntityCard
+                    key={category.id}
+                    id={category.id}
+                    name={category.name}
+                    image={category.image}
+                  />
+                ))}
+            </EntityCardContainer>
+          </div>
+          <div className="col">
+            <EntityCardContainer
+              title={`Products (${this.context.entities.products.length})`}
+              entityType="product"
+            >
+              {this.context.entities.products.map(product => (
                 <EntityCard
-                  key={category.id}
-                  id={category.id}
-                  name={category.name}
-                  image={category.image}
+                  key={product.id}
+                  id={product.id}
+                  name={product.name}
+                  image={product.image}
+                  description={product.description}
+                  price={product.price}
                 />
               ))}
             </EntityCardContainer>
           </div>
-          {this.context.entities.categories ? (
-            <div className="col">
-              <EntityCardContainer
-                title={`Products (${this.context.entities.products.length})`}
-                entityType="product"
-              >
-                {this.context.entities.products.map(product => (
-                  <EntityCard
-                    key={product.id}
-                    id={product.id}
-                    name={product.name}
-                    image={product.image}
-                    description={product.description}
-                    price={product.price}
-                  />
-                ))}
-              </EntityCardContainer>
-            </div>
-          ) : null}
         </FlexContainer>
       </AdminLayout>
     );
