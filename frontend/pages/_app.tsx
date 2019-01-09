ae571f5bb726dfc7ab1f59f4eccd34d29a0bbd87
Simo Aleksandrov <simo3003@me.com>
2019-01-09 09:02:28 +0200

    Add CartContextProvider to _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,6 +2,7 @@ import App, { Container, NextAppContext } from 'next/app';
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
 import EntityContextProvider from '../components/AdminContextProvider';
+import CartContextProvider from '../components/CartContextProvider';
 
 const GlobalStyle = createGlobalStyle`
   html,
@@ -29,12 +30,14 @@ export default class MyApp extends App {
     const { Component, pageProps } = this.props;
 
     return (
-      <EntityContextProvider>
-        <Container>
-          <GlobalStyle />
-          <Component {...pageProps} />
-        </Container>
-      </EntityContextProvider>
+      <CartContextProvider>
+        <EntityContextProvider>
+          <Container>
+            <GlobalStyle />
+            <Component {...pageProps} />
+          </Container>
+        </EntityContextProvider>
+      </CartContextProvider>
     );
   }
 }
