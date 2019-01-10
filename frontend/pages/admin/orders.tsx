a256f35bf290ec22918290a04c1e00c1aedd7f1d
Lyubo Lyubchev <lyubo_2317@abv.bg>
2019-01-10 03:40:26 +0200

    Setup socket io connection to backend

--- frontend/pages/admin/orders.tsx
+++ frontend/pages/admin/orders.tsx
@@ -1,13 +1,30 @@
 import { Component } from 'react';
+import io from 'socket.io-client';
 import AdminLayout from '../../components/AdminLayout';
 import FullHeightContainer from '../../components/FullHeightContainer';
 import withAuth from '../../components/withAuth';
+import OrderCard from '../../components/OrderCard';
+import { BACKEND_URL } from '../../config';
 
 class Orders extends Component {
+  socket: SocketIOClient.Socket;
+
+  componentDidMount() {
+    this.socket = io(`${BACKEND_URL}`);
+    this.socket.on('orders_fetched', (data: any) => {
+      console.log(data);
+      this.socket.emit('place_order', { order: ' ei kopele' });
+    });
+    this.socket.on('order_placed', (data: any) => {
+      console.log('this comes from order_placed', data);
+    });
+  }
   render() {
     return (
       <FullHeightContainer>
-        <AdminLayout selectedKey="orders">Orders go here...</AdminLayout>
+        <AdminLayout selectedKey="orders">
+          <OrderCard orderId={4}>tuka idvat groznite</OrderCard>
+        </AdminLayout>
       </FullHeightContainer>
     );
   }
