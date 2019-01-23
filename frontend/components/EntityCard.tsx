456d5244ddcb1468a14006d03ac234d90a57918f
Lyubo Lyubchev <lyubo_2317@abv.bg>
2019-01-23 15:58:34 +0200

    Remove unnecessary imports from EntityCard

--- frontend/components/EntityCard.tsx
+++ frontend/components/EntityCard.tsx
@@ -1,5 +1,5 @@
-import { Card, Icon, Popconfirm, message } from 'antd';
-import React, { Component } from 'react';
+import { Card, Icon, Popconfirm } from 'antd';
+import React from 'react';
 import styled from 'styled-components';
 import ActionButton from './ActionButton';
 import PriceBadge from './PriceBadge';
