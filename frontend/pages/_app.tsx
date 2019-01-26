03982e7827c893e7fe326afd1648de65e6968530
Lyubo Lyubchev <lyubo_2317@abv.bg>
2019-01-26 17:01:48 +0200

    Add page transitions

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,7 @@
-import App, { Container, NextAppContext } from 'next/app';
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
+import App, { Container, NextAppContext } from 'next/app';
+import styled, { createGlobalStyle } from 'styled-components';
+import { PageTransition } from 'next-page-transitions';
 import EntityContextProvider from '../components/AdminContextProvider';
 import CartContextProvider from '../components/CartContextProvider';
 
@@ -15,6 +16,32 @@ const GlobalStyle = createGlobalStyle`
   }
 
   #__next {
+    height: 100%;
+  }
+
+  .page-transition-enter {
+    height: 100%;
+    opacity: 0;
+  }
+
+  .page-transition-enter-active {
+    height: 100%;
+    opacity: 1;
+    transition: opacity 300ms;
+  }
+
+  .page-transition-exit {
+    height: 100%;
+    opacity: 1;
+  }
+
+  .page-transition-exit-active {
+    height: 100%;
+    opacity: 0;
+    transition: opacity 300ms;
+  }
+
+  .page-transition-enter-done {
     height: 100%;
   }
 `;
@@ -38,7 +65,9 @@ export default class MyApp extends App {
         <EntityContextProvider>
           <Container>
             <GlobalStyle />
-            <Component {...pageProps} />
+            <PageTransition timeout={200} classNames="page-transition">
+              <Component {...pageProps} />
+            </PageTransition>
           </Container>
         </EntityContextProvider>
       </CartContextProvider>
