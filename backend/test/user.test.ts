322f111186c96e4e1c81e52ef83d15e5a1b1a8ee
Simo Aleksandrov <simo3003@me.com>
2019-02-03 03:30:03 +0200

    Add more tests

--- backend/test/user.test.ts
+++ backend/test/user.test.ts
@@ -15,10 +15,10 @@ beforeAll(async () => {
   userRepository = getRepository(User);
 });
 
-describe('Registering users', () => {
+describe('Registering users with invalid fields', () => {
   it('adds a valid user to the database when registering', async () => {
     const userCredentials: Partial<User> = {
-      email: faker.internet.email(),
+      email: faker.internet.exampleEmail(),
       name: faker.name.findName(),
       password: 'FAKEpassword123'
     };
@@ -38,11 +38,13 @@ describe('Registering users', () => {
 
     expect(users).toMatchObject([data]);
   });
+});
 
+describe('Not registering users with invalid fields', () => {
   it('throws an error when registering a user with an invalid email', async () => {
     const userCredentials: Partial<User> = {
       email: 'this_is-not_an_email123',
-      name: 'John Doe',
+      name: faker.name.findName(),
       password: 'FAKEpassword123'
     };
 
@@ -53,6 +55,148 @@ describe('Registering users', () => {
         expect(res.status).toEqual(400);
         expect(res.body).toEqual({
           errors: ['email must be an email'],
+          name: 'NotValidError',
+          message: 'User not valid!'
+        });
+      });
+  });
+
+  it('throws an error when registering a user with an invalid password', async () => {
+    const userCredentials: Partial<User> = {
+      email: faker.internet.exampleEmail(),
+      name: faker.name.findName(),
+      password: 'remember'
+    };
+
+    return request(server)
+      .post('/auth/register')
+      .send(userCredentials)
+      .then((res: Response) => {
+        expect(res.status).toEqual(400);
+        expect(res.body).toEqual({
+          errors: [
+            // tslint:disable
+            'Password must contain at least 1 lowercase alphabetical character, 1 numeric character and be at least 8 characters long'
+            // tslint:enable
+          ],
+          name: 'NotValidError',
+          message: 'User not valid!'
+        });
+      });
+  });
+
+  it('throws an error when registering a user with all fields invalid', async () => {
+    const userCredentials: Partial<User> = {
+      email: 'not-an-email',
+      name: '',
+      password: 'badpass'
+    };
+
+    return request(server)
+      .post('/auth/register')
+      .send(userCredentials)
+      .then((res: Response) => {
+        expect(res.status).toEqual(400);
+        expect(res.body).toEqual({
+          errors: [
+            'name must be longer than or equal to 1 characters',
+            'email must be an email',
+            // tslint:disable
+            'Password must contain at least 1 lowercase alphabetical character, 1 numeric character and be at least 8 characters long'
+            // tslint:enable
+          ],
+          name: 'NotValidError',
+          message: 'User not valid!'
+        });
+      });
+  });
+});
+
+describe('Not registering users with empty fields', () => {
+  it('throws an error when registering a user with an empty email', async () => {
+    const userCredentials: Partial<User> = {
+      email: '',
+      name: faker.name.findName(),
+      password: 'FAKEpassword123'
+    };
+
+    return request(server)
+      .post('/auth/register')
+      .send(userCredentials)
+      .then((res: Response) => {
+        expect(res.status).toEqual(400);
+        expect(res.body).toEqual({
+          errors: ['email must be an email'],
+          name: 'NotValidError',
+          message: 'User not valid!'
+        });
+      });
+  });
+
+  it('throws an error when registering a user with an empty name', async () => {
+    const userCredentials: Partial<User> = {
+      email: faker.internet.exampleEmail(),
+      name: '',
+      password: 'FAKEpassword123'
+    };
+
+    return request(server)
+      .post('/auth/register')
+      .send(userCredentials)
+      .then((res: Response) => {
+        expect(res.status).toEqual(400);
+        expect(res.body).toEqual({
+          errors: ['name must be longer than or equal to 1 characters'],
+          name: 'NotValidError',
+          message: 'User not valid!'
+        });
+      });
+  });
+
+  it('throws an error when registering a user with an empty password', async () => {
+    const userCredentials: Partial<User> = {
+      email: faker.internet.exampleEmail(),
+      name: 'John Doe',
+      password: ''
+    };
+
+    return request(server)
+      .post('/auth/register')
+      .send(userCredentials)
+      .then((res: Response) => {
+        expect(res.status).toEqual(400);
+        expect(res.body).toEqual({
+          errors: [
+            // tslint:disable
+            'Password must contain at least 1 lowercase alphabetical character, 1 numeric character and be at least 8 characters long'
+            // tslint:enable
+          ],
+          name: 'NotValidError',
+          message: 'User not valid!'
+        });
+      });
+  });
+
+  it('throws an error when registering a user with all fields empty', async () => {
+    const userCredentials: Partial<User> = {
+      email: '',
+      name: '',
+      password: ''
+    };
+
+    return request(server)
+      .post('/auth/register')
+      .send(userCredentials)
+      .then((res: Response) => {
+        expect(res.status).toEqual(400);
+        expect(res.body).toEqual({
+          errors: [
+            'name must be longer than or equal to 1 characters',
+            'email must be an email',
+            // tslint:disable
+            'Password must contain at least 1 lowercase alphabetical character, 1 numeric character and be at least 8 characters long'
+            // tslint:enable
+          ],
           name: 'NotValidError',
           message: 'User not valid!'
         });
