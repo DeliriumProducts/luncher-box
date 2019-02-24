d787f89f2225b9401b01eaca17598810b1fe9cf6
Simo Aleksandrov <simo3003@me.com>
2019-02-24 21:12:10 +0200

    Update script

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,4 +6,6 @@ sudo systemctl start redis
 xfce4-terminal --working-directory=$(pwd)/../frontend &
 xfce4-terminal --working-directory=$(pwd)/../backend &
 
+$BROWSER "http://$(hostname -i | xargs):3000"
+
 code ../ &
