cd5905b198c9886ec27862fc85634d3f6c49afa6
Simo Aleksandrov <simo3003@me.com>
2019-03-06 21:34:58 +0200

    Fix dropAndSync

--- backend/src/connections/db.ts
+++ backend/src/connections/db.ts
@@ -1,7 +1,15 @@
 import { createConnection, getConnection } from 'typeorm';
 import { dbConfig } from '../config';
 
-export const dbConnection = async (dropAndSync: boolean = false) => {
+export const dbConnection = async (dropAndSync?: boolean) => {
+  if (!dropAndSync) {
+    if (dbConfig.synchronize) {
+      dropAndSync = dbConfig.synchronize;
+    } else {
+      dropAndSync = false;
+    }
+  }
+
   try {
     return getConnection('default');
   } catch (e) {
