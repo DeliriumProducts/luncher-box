5f94ce11f1c0de60f0e897d47f28f858a7c8d79c
Simo Aleksandrov <simo3003@me.com>
2019-03-24 00:01:06 +0200

    Add supersonic deck

--- presentation/supersonic/src/theme.js
+++ presentation/supersonic/src/theme.js
@@ -4,9 +4,13 @@ export default {
   ...theme,
   transitionTimingFunction: "cubic-bezier(1, 0, 0, 1)",
   transitionDuration: ".8s",
+  fontSize: "0.5rem",
   colors: {
     text: "#D56AA0",
     link: "#0ff",
     background: "#FCF8ED"
+  },
+  ul: {
+    listStyleType: "none"
   }
 };
