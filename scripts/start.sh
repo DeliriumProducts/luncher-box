a0517922fe4d0cfb25a5aea30ed64b153a9c53ad
Simo Aleksandrov <simo3003@me.com>
2019-03-27 21:16:12 +0200

    Update start script to use tabs

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,8 +3,7 @@
 sudo systemctl start mysqld
 sudo systemctl start redis
 
-xfce4-terminal --working-directory=$(pwd)/../frontend &
-xfce4-terminal --working-directory=$(pwd)/../backend &
+xfce4-terminal -T luncherbox-frontend --working-directory=$(pwd)/../frontend --tab  -T luncherbox-backend --working-directory=$(pwd)/../backend --tab &
 
 $BROWSER "http://$(hostname -i | xargs):3000"
 
