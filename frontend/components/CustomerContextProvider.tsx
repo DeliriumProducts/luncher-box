41902d02b23aae2728c15961769c14b956447dac
Simo Aleksandrov <simo3003@me.com>
2019-04-04 22:03:29 +0300

    Add comments to the CustomerContextProvider

--- frontend/components/CustomerContextProvider.tsx
+++ frontend/components/CustomerContextProvider.tsx
@@ -28,8 +28,11 @@ class CustomerContextProvider extends Component<Props, State> {
     hasFinishedSyncing: false
   };
 
+  /**
+   * Gets the items from localForage and updates the Context Values with them
+   */
   syncWithLocalForage = async (): Promise<void> => {
-    const currentOrder: Order = (await localForage.getItem('currentOrder')) || {
+    const order: Order = (await localForage.getItem('order')) || {
       products: [],
       comment: '',
       table: ''
@@ -42,10 +45,10 @@ class CustomerContextProvider extends Component<Props, State> {
     const orderHistory: Order[] =
       (await localForage.getItem('orderHistory')) || [];
 
-    currentOrder.table = table;
+    order.table = table;
 
     this.setState({
-      order: currentOrder,
+      order,
       totalAmount,
       table,
       orderHistory,
@@ -53,6 +56,9 @@ class CustomerContextProvider extends Component<Props, State> {
     });
   };
 
+  /**
+   * Clears the current order (removes products and comment) and resets the totalAmount
+   */
   clear = async () => {
     this.setState(prevState => ({
       order: {
@@ -64,17 +70,21 @@ class CustomerContextProvider extends Component<Props, State> {
     }));
 
     return Promise.all([
-      localForage.removeItem('currentOrder'),
+      localForage.removeItem('order'),
       localForage.removeItem('totalAmount')
     ]);
   };
 
+  /**
+   * Increments / adds the quantity of a given product in the current order
+   */
   increment = async (product: Product) => {
     this.setState(
       prevState => {
         let isNewProduct = true;
 
         const editedProducts: Product[] = [];
+
         prevState.order.products.forEach(p => {
           if (p.id === product.id) {
             isNewProduct = false;
@@ -102,12 +112,15 @@ class CustomerContextProvider extends Component<Props, State> {
         };
       },
       () => {
-        localForage.setItem('currentOrder', this.state.order);
+        localForage.setItem('order', this.state.order);
         localForage.setItem('totalAmount', this.state.totalAmount);
       }
     );
   };
 
+  /**
+   * Decrements / removes the quantity of a given product in the current order
+   */
   decrement = async (product: Product) => {
     this.setState(
       prevState => {
@@ -147,21 +160,27 @@ class CustomerContextProvider extends Component<Props, State> {
         };
       },
       () => {
-        localForage.setItem('currentOrder', this.state.order);
+        localForage.setItem('order', this.state.order);
         localForage.setItem('totalAmount', this.state.totalAmount);
       }
     );
   };
 
+  /**
+   * Adds a comment to the current order
+   */
   comment = (comment: string) => {
     this.setState(
       prevState => ({ order: { ...prevState.order, comment } }),
       () => {
-        localForage.setItem('currentOrder', this.state.order);
+        localForage.setItem('order', this.state.order);
       }
     );
   };
 
+  /**
+   * Sets the table of the current order and in localForage
+   */
   setTable = (id: string) => {
     this.setState(
       prevState => ({
@@ -172,12 +191,15 @@ class CustomerContextProvider extends Component<Props, State> {
         table: id
       }),
       () => {
-        localForage.setItem('currentOrder', this.state.order);
+        localForage.setItem('order', this.state.order);
         localForage.setItem('table', id);
       }
     );
   };
 
+  /**
+   * Updates a given order inside the orderHistory
+   */
   updateOrderHistory = (order: Order) => {
     this.setState(
       prevState => {
@@ -203,12 +225,18 @@ class CustomerContextProvider extends Component<Props, State> {
     );
   };
 
+  /**
+   * Completely overwrites the orderHistory
+   */
   overwriteOrderHistory = (orders: Order[]) => {
     this.setState({ orderHistory: orders }, async () => {
       localForage.setItem('orderHistory', this.state.orderHistory);
     });
   };
 
+  /**
+   * Pushes a new order to orderHistory
+   */
   pushOrderHistory = (order: Order) => {
     this.setState(
       prevState => {
@@ -225,6 +253,9 @@ class CustomerContextProvider extends Component<Props, State> {
     );
   };
 
+  /**
+   * Sync whenever the component loads
+   */
   componentDidMount = () => {
     this.syncWithLocalForage();
   };
