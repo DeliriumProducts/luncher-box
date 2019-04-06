5f7df99b0d3290470c55eef2af1405dbd3a1571a
Simo Aleksandrov <simo3003@me.com>
2019-04-07 02:20:07 +0300

    Make app/index and app/category fetch on the backend using getInitalProps

--- frontend/components/withRouter.tsx
+++ frontend/components/withRouter.tsx
@@ -1,10 +1,13 @@
-import { NextContext } from 'next';
-import React, { Component, ComponentClass } from 'react';
+import { NextComponentClass, NextContext, NextFunctionComponent } from 'next';
+import React, { Component } from 'react';
 
 /**
- * Alternative to the withRouter from next/router, until it gets migrated to the new context API
+ * Alternative to the withRouter from next/router, until it gets migrated to the new context API.
+ * It is to be used when you don't want / need the getInitalProps function, as this overrides it.
  */
-const withRouter = <T extends object>(C: ComponentClass<T>) =>
+const withRouter = <T extends object>(
+  C: NextFunctionComponent<T> | NextComponentClass<T>
+) =>
   class extends Component<T> {
     static async getInitialProps({ query }: NextContext) {
       return {
