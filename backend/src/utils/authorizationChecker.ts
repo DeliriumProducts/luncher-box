37141acdae29c17b1c2f6af8936760a62fe97434
Lyubo Lyubchev <lyubo_2317@abv.bg>
2019-04-22 22:41:32 +0300

    Implement authorization for user roles

--- backend/src/utils/authorizationChecker.ts
+++ backend/src/utils/authorizationChecker.ts
@@ -1,7 +1,15 @@
 import { Request } from 'express';
 import { Action } from 'routing-controllers';
+import { Role } from '../types';
+import { User } from '../entities';
 
-export const authorizationChecker = async (action: Action) => {
+export const authorizationChecker = async (action: Action, r: Role[]) => {
   const req: Request = action.request;
-  return req.isAuthenticated();
+  const user: User = req.user;
+
+  if (!r.length) {
+    return req.isAuthenticated();
+  } else {
+    return (user.role === 'Admin' || r.includes(user.role)) && req.isAuthenticated();
+  }
 };
