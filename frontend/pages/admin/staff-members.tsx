735d088d50b4838b726377b24d0dd835f783f8f9
Lyubo Lyubchev <lyubo_2317@abv.bg>
2019-04-26 14:35:14 +0300

    Add filtering option to the the staff member table

--- frontend/pages/admin/staff-members.tsx
+++ frontend/pages/admin/staff-members.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import Head from 'next/head';
 import withAuth from '../../components/withAuth';
 import styled from 'styled-components';
-import { Table, message } from 'antd';
+import { Table, message, Tag, Input, Button, Icon } from 'antd';
 import { NextFunctionComponent, NextContext } from 'next';
 import { User } from '../../interfaces';
 import { StaffAPI } from '../../api';
+import { Role } from '../../types';
+import { THEME_VARIABLES } from '../../config';
+import Highlighter from 'react-highlight-words';
 
 interface Props {
   staff: User[] | [];
@@ -41,31 +44,121 @@ const FlexContainer = styled.div`
   }
 `;
 
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name'
-  },
-  {
-    title: 'Email',
-    dataIndex: 'email',
-    key: 'email'
-  },
-  {
-    title: 'Role',
-    dataIndex: 'role',
-    key: 'role'
-  },
-  {
-    title: 'Verified',
-    dataIndex: 'isVerified',
-    key: 'isVerified',
-    render: value => (value ? 'Yes' : 'No')
-  }
-];
-
 const StaffMembers: NextFunctionComponent<Props> = ({ err, staff }) => {
+  const [searchText, setSearchText] = React.useState('');
+  const searchInput = React.useRef<Input | null>();
+
+  const getColumnSearchProps = dataIndex => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters
+    }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={node => {
+            searchInput.current = node;
+          }}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={e => {
+            setSelectedKeys(e.target.value ? [e.target.value] : []);
+          }}
+          onPressEnter={e => handleSearch(selectedKeys, confirm)}
+          style={{ width: 188, marginBottom: 8, display: 'block' }}
+        />
+        <Button
+          type="primary"
+          onClick={() => handleSearch(selectedKeys, confirm)}
+          icon="search"
+          size="small"
+          style={{ width: 90, marginRight: 8 }}
+        >
+          Search
+        </Button>
+        <Button
+          onClick={() => handleReset(clearFilters)}
+          size="small"
+          style={{ width: 90 }}
+        >
+          Reset
+        </Button>
+      </div>
+    ),
+    filterIcon: filtered => (
+      <Icon
+        type="search"
+        style={{
+          color: filtered ? THEME_VARIABLES['@primary-color'] : undefined
+        }}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes(value.toLowerCase()),
+    onFilterDropdownVisibleChange: visible => {
+      if (visible) {
+        setTimeout(() => {
+          searchInput.current!.focus();
+        }, 150);
+      }
+    }
+  });
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      ...getColumnSearchProps('name')
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+      ...getColumnSearchProps('email')
+    },
+    {
+      title: 'Role',
+      dataIndex: 'role',
+      key: 'role',
+      render: (role: Role) => {
+        const colors = {
+          Waiter: 'green',
+          Cook: THEME_VARIABLES['@primary-color'],
+          Admin: 'volcano'
+        };
+
+        return (
+          <span>
+            <Tag color={colors[role]} key={role}>
+              {role.toUpperCase()}
+            </Tag>
+          </span>
+        );
+      }
+    },
+    {
+      title: 'Verified',
+      dataIndex: 'isVerified',
+      key: 'isVerified',
+      render: value => (value ? 'Yes' : 'No')
+    }
+  ];
+
+  const handleSearch = (selectedKeys, confirm) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+  };
+
+  const handleReset = clearFilters => {
+    clearFilters();
+    setSearchText('');
+  };
+
   /**
    * Show only on cDM
    */
