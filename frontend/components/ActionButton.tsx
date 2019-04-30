9f289c2773e037e6b63bbd5b8f488c4534e9e658
Lyubo Lyubchev <lyubo_2317@abv.bg>
2019-04-30 18:27:44 +0300

    Spread props passed to ActionButton

--- frontend/components/ActionButton.tsx
+++ frontend/components/ActionButton.tsx
@@ -1,9 +1,13 @@
 import { Button } from 'antd';
 import styled from 'styled-components';
 
-const ActionButton: any = styled(Button)`
+const StyledButton: any = styled(Button)`
   border: none;
   box-shadow: 0 2px 2px rgba(0, 0, 0, 0.12);
 `;
+
+const ActionButton: React.FunctionComponent<any> = props => (
+  <StyledButton {...props}>{props.children}</StyledButton>
+);
 
 export default ActionButton;
