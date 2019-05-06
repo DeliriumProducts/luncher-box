9b6d0dda16f5a660c3062a0f9433905adf259b12
Simo Aleksandrov <simo3003@me.com>
2019-05-06 03:44:41 +0300

    Add PageHeader to My Orders page

--- frontend/pages/admin/restaurant-load.tsx
+++ frontend/pages/admin/restaurant-load.tsx
@@ -1,28 +1,5 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
 import { withAuth } from '../../hocs';
-
-const FlexContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  width: 100%;
-
-  .col {
-    flex: 1;
-    max-width: 70%;
-    height: 100%;
-    margin: auto;
-  }
-
-  @media (max-width: 768px) {
-    .col {
-      max-width: 100%;
-    }
-
-    flex-direction: column;
-  }
-`;
 
 class RestaurantLoad extends Component {
   render() {
--- frontend/pages/app/orders.tsx
+++ frontend/pages/app/orders.tsx
@@ -1,16 +1,26 @@
-import { Empty } from 'antd';
+import { Empty, PageHeader } from 'antd';
 import Head from 'next/head';
 import React, { useContext } from 'react';
 import styled from 'styled-components';
 import Order from '../../components/OrderCard';
 import { CustomerContext } from '../../context';
 
+const StyledPageHeader = styled(PageHeader)`
+  background-color: #fafafa;
+  border-radius: 7px;
+  flex: 1;
+
+  @media (max-width: 480px) {
+    border-radius: 0;
+    margin: 0;
+  }
+`;
+
 const FlexContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   align-items: center;
   justify-content: center;
-  padding: 2rem;
   background-color: #fafafa;
   border-radius: 7px;
   box-shadow: 0 20px 24px -18px rgba(0, 0, 0, 0.31);
@@ -45,7 +55,22 @@ export default () => {
       <Head>
         <title>Orders • LuncherBox</title>
       </Head>
-      <FlexContainer>{data}</FlexContainer>
+      <FlexContainer>
+        <StyledPageHeader
+          title={
+            <h1>
+              <strong>My orders</strong>
+            </h1>
+          }
+          subTitle={
+            <h3>
+              <strong>({orderHistory.length})</strong>
+            </h3>
+          }
+        >
+          {data}
+        </StyledPageHeader>
+      </FlexContainer>
     </>
   );
 };
