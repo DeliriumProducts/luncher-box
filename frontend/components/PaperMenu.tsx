94f9f2c25ee06ee3a5941c45d51330e21b0a5853
Simo Aleksandrov <simo3003@me.com>
2019-05-20 00:41:42 +0300

    Finally got images to display in pdfs

--- frontend/components/PaperMenu.tsx
+++ frontend/components/PaperMenu.tsx
@@ -1,4 +1,12 @@
-import { Document, Page, StyleSheet, Text, View } from '@react-pdf/renderer';
+import {
+  Document,
+  Font,
+  Image,
+  Page,
+  StyleSheet,
+  Text,
+  View
+} from '@react-pdf/renderer';
 import React from 'react';
 import { Category, Product } from '../interfaces';
 
@@ -11,6 +19,14 @@ const styles = StyleSheet.create({
     margin: 10,
     padding: 10,
     flexGrow: 1
+  },
+  text: {
+    fontFamily: 'Oswald',
+    color: '#212121'
+  },
+  image: {
+    width: 10,
+    height: 10
   }
 });
 
@@ -19,7 +35,31 @@ interface Props {
   products: Product[];
 }
 
-// Create Document Component
+const ProductItem: React.FunctionComponent<Partial<Product>> = ({
+  name,
+  description,
+  image
+}) => {
+  return (
+    <View>
+      <Text>{name}</Text>
+      <Image
+        style={styles.image}
+        source={{
+          uri: image!,
+          method: 'GET',
+          mode: 'cors',
+          referrerPolicy: 'no-referrer-when-downgrade',
+          headers: {
+            'Access-Control-Allow-Origin': '*'
+          },
+          body: null
+        }}
+      />
+    </View>
+  );
+};
+
 const PaperMenu: React.FunctionComponent<Props> = ({
   categories,
   products
@@ -34,12 +74,17 @@ const PaperMenu: React.FunctionComponent<Props> = ({
         </View>
         <View style={styles.section}>
           {products.map(p => {
-            return <Text key={p.id}>{p.name}</Text>;
+            return <ProductItem key={p.id} {...p} />;
           })}
         </View>
       </Page>
     </Document>
   );
 };
+
+Font.register({
+  family: 'Oswald',
+  src: 'https://fonts.gstatic.com/s/oswald/v13/Y_TKV6o8WovbUd3m_X9aAA.ttf'
+});
 
 export default PaperMenu;
