86963f53df0ccb385006f1578c069b43697ab74f
Simo Aleksandrov <simo3003@me.com>
2019-05-21 18:29:22 +0300

    Pub/sub messages across multiple backend instances using redis for socket messages

--- backend/src/config/socket.io.ts
+++ backend/src/config/socket.io.ts
@@ -1,6 +1,8 @@
 import http from 'http';
 import 'reflect-metadata';
 import createSocketIoServer from 'socket.io';
+import redisAdapter from 'socket.io-redis';
+import { REDIS_HOST, REDIS_PORT } from '.';
 import { app } from './express';
 
 /**
@@ -11,6 +13,13 @@ const server = http.createServer(app);
 /**
  * Pass the express instance / http server to socket.io
  */
+
 const io = createSocketIoServer(server);
+io.adapter(
+  redisAdapter({
+    host: REDIS_HOST,
+    port: REDIS_PORT
+  })
+);
 
 export { io, server };
